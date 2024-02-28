a0e2b71193a09e679bab89b43f85f00781f2da4f
Giulio Procopio <giulioprocopioo@gmail.com>
2024-02-28 17:37:02 +0100

    Fix force option OS misdetection bug

--- src/winclear/__init__.py
+++ src/winclear/__init__.py
@@ -11,7 +11,7 @@ def clear(force: bool = False) -> None:
     """Flush the Windows command line output."""
 
     # This is not complete nor correct, but in most cases it should work.
-    if platform == 'win32' and not force:
+    if platform == 'win32' or force:
         exit_code = os.system('cls')
         if exit_code != 0:
             raise OSError(f'could not clear console (exit code: {exit_code})')
