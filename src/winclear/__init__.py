f783b63e244843eebba95281ff9767780161d497
Giulio Procopio <giulioprocopioo@gmail.com>
2024-02-28 17:06:55 +0100

    Switch to dynamic Git to Python versioning

--- src/winclear/__init__.py
+++ src/winclear/__init__.py
@@ -1,6 +1,11 @@
 import os
 from sys import platform
 
+try:
+    from ._version import __version__  # type: ignore
+except ImportError:
+    __version__ = '0.0.0'
+
 
 def clear(force: bool = False) -> None:
     """Flush the Windows command line output."""
